e2243badb14ff08dbebdb017bc481b6f41c3e122
Martin Lehmann <martin@folio.no>
2019-01-04 23:23:03 +0100

    add test that only receipt filter works

--- test.ts
+++ test.ts
@@ -21,9 +21,13 @@ const food = t(tesco, coop);
 const transport = t(underground, taxi);
 const internet = t(google);
 
+const categories = { all, food, transport, internet };
+
 // by receipt
 const present = t(tesco, underground, google, taxi);
 const missing = t(coop);
+
+const receipts = { all, present, missing };
 
 function t(...indices: number[]): Transaction[] {
   return indices.map(i => transactions[i]);
@@ -47,7 +51,19 @@ test('only category filter', () => {
   });
 
   expect(counts).toEqual({
-    categories: { all, food, transport, internet },
+    categories,
     receipts: { all: t(tesco, coop), present: t(tesco), missing: t(coop) },
+  });
+});
+
+test('only receipt filter', () => {
+  const counts = countTransactions(transactions, {
+    receipt: 'present',
+    category: 'all',
+  });
+
+  expect(counts).toEqual({
+    categories: { all: present, food: t(tesco), transport, internet },
+    receipts,
   });
 });
